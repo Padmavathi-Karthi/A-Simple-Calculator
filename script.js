9ccddb1711356cd9b4e945275fed5616ad62b602
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-02-04 16:31:09 +0000

    js logic added

---
+++ script.js
@@ -0,0 +1,13 @@
+let equal_pressed = 0;
+
+// Refer all buttons excluding C and CE
+
+let button_input = document.querySelectorAll(".input-button");
+
+// Refer input, equal, clear and backspace
+
+let input = document.getElementById("input");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+
